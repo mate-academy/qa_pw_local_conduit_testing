5938897702f10635f8c15f358cec8e1c08d55ebd
sr.veremienko@gmail.com <sr.veremienko@gmail.com>
2025-08-24 20:10:52 +0300

    Added local env and routs

--- src/ui/constants/pageRoutes.js
+++ src/ui/constants/pageRoutes.js
@@ -1,6 +1,18 @@
-export const ROUTES = {
-  home: '/',
-  articleEditor: '/editor',
-  login: `/user/login`,
-  register: `/user/register`,
+const ENV_TYPE = process.env.ENV_TYPE || 'local';
+
+const routesByEnv = {
+  local: {
+    home: '/',
+    login: '#/login',
+    register: '#/register',
+    articleEditor: '#/editor',
+  },
+  staging: {
+    home: '/',
+    login: '/user/login',
+    register: '/user/register',
+    articleEditor: '/editor',
+  },
 };
+
+export const ROUTES = routesByEnv[ENV_TYPE];
